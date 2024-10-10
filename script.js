4fb759f5e4ca76597657b6d587e031b40a8df1c2
Abdosamir10 <155080502+Abdosamir10@users.noreply.github.com>
2024-10-10 14:40:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,42 +9,41 @@ const prevtBtn = document.querySelector('.prev-btn');
 
 let colors = ['#0E5BA5', '#E16795', '#E2BE59', '#DB2F3D', '#171f2b', '#63B058', ];
 
-let indexSlider = 0 ;
-let index = 0 ;
+let indexSlider = 0;
+let index = 0;
 
 const slider = () => {
-    imgSlider.style.transform  = ` rotate (${indexSlider * 60}deg)`;
+  imgSlider.style.transform = `rotate(${indexSlider * 60}deg)`;
 
-    items.forEach(item => {
-        item.style.transform  = ` rotate (${indexSlider * -60}deg)`;
-    });
+  items.forEach(item => {
+    item.style.transform = `rotate(${indexSlider * -60}deg)`;
+  });
 
-    document.querySelector('.img-item.active').classList.remove('active')
-    imgItems[index].classList.add('active');
+  document.querySelector('.img-item.active').classList.remove('active');
+  imgItems[index].classList.add('active');
 
-    document.querySelector('.info-item.active').classList.remove('active')
-    infoItems[index].classList.add('active');
+  document.querySelector('.info-item.active').classList.remove('active');
+  infoItems[index].classList.add('active');
 
-    document.body.style.background =colors[index];
+  document.body.style.background = colors[index];
 }
-nextBtn.addEventListener('click',() => {
-    indexSlider++;
 
-    index++;
-    if(index > imgItems.length -1){
-        index = 0;
-    }
+nextBtn.addEventListener('click', () => {
+  indexSlider++;
+  index++;
+  if (index > imgItems.length - 1) {
+    index = 0;
+  }
 
-    slider();
+  slider();
 });
-prevtBtn.addEventListener('click',() => {
-    indexSlider--;
 
-    index--;
-    if(index < imgItems.length -1){
-        index = 0;
-    }
+prevtBtn.addEventListener('click', () => {
+  indexSlider--;
+  index--;
+  if (index < 0) { // Corrected condition here
+    index = imgItems.length - 1;
+  }
 
-    
-    slider();
-    });
+  slider();
+});
